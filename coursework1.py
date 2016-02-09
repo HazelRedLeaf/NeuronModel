a37de627c72e180a466b79f578681fa7a4ac1e09
HazelRedLeaf <mm13354@my.bristol.ac.uk>
2016-02-09 18:29:18 +0000

    Initial commit

---
+++ coursework1.py
@@ -0,0 +1,17 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+################################
+# variables
+#
+# tau_m = 10ms
+# E_l = V_r = -70 mV
+# V_t = -40 mV
+# R_m = 10 MOmega
+# I_e = 3.1 nA
+tau_m = 10
+E_l = V_r = -10
+V_t = -40
+R_m = 10
+I_e = 3.1
